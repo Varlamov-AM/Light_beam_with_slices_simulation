1d0b87737625c460d0a9452d364d71eefc38c1ad
Antony <Varlamov.AM@phystech.edu>
2025-09-12 15:18:21 +0300

    Add realistic spectrum handling to physical model of project

--- include/LightSimulation.hpp
+++ include/LightSimulation.hpp
@@ -18,8 +18,9 @@ namespace Project {
     class LightSource {
     public:
         enum class SourceType { PlanePolarized };
+        static constexpr int n_beams = 41;
         LightSource(SourceType type = SourceType::PlanePolarized, double intensity = 1.0, double polarization_angle = 0.0);
-        std::vector<std::array<LightBeam, 3>> generate_beams() const;
+        std::vector<std::array<LightBeam, n_beams>> generate_beams() const;
 
     private:
         SourceType type_;
@@ -121,6 +122,65 @@ namespace Project {
     public:
         PhysicalEngine(World& world, LightSource& light_source);
         std::vector<std::vector<std::array<double, 3>>> simulate();
+        static constexpr int lambda_arr_len = LightSource::n_beams;
+    
+        std::vector<double> apply_xyz_filter(std::vector<double> intensity);
+
+        static constexpr double lambda_arr[lambda_arr_len] = 
+            {
+            380, 390, 400, 410, 420, 430, 440, 450, 460, 470,
+            480, 490, 500, 510, 520, 530, 540, 550, 560, 570,
+            580, 590, 600, 610, 620, 630, 640, 650, 660, 670,
+            680, 690, 700, 710, 720, 730, 740, 750, 760, 770, 
+            780
+            };
+
+        static constexpr double x_filter[lambda_arr_len] = 
+            {
+            0.0014, 0.0042, 0.0143, 0.0435, 0.1344,
+            0.2839, 0.3483, 0.3362, 0.2908, 0.1954,
+            0.0956, 0.0320, 0.0049, 0.0093, 0.0633,
+            0.1655, 0.2904, 0.4334, 0.5945, 0.7621,
+            0.9163, 1.0263, 1.0622, 1.0026, 0.8544,
+            0.6424, 0.4479, 0.2835, 0.1649, 0.0874,
+            0.0468, 0.0227, 0.0114, 0.0058, 0.0029,
+            0.0014, 0.0007, 0.0003, 0.0002, 0.0001,
+            0.
+            };
+
+        static constexpr double y_filter[lambda_arr_len] = 
+            {
+            0.0000, 0.0001, 0.0004, 0.0012, 0.0040,
+            0.0116, 0.0230, 0.0380, 0.0600, 0.0910,
+            0.1390, 0.2080, 0.3230, 0.5030, 0.7100,
+            0.8620, 0.9540, 0.9950, 0.9950, 0.9520, 
+            0.8700, 0.7570, 0.6310, 0.5030, 0.3810,
+            0.2650, 0.1750, 0.1070, 0.0610, 0.0320,
+            0.0170, 0.0082, 0.0041, 0.0021, 0.0010,
+            0.0005, 0.0003, 0.0001, 0.0001, 0.0000,
+            0.
+            };
+
+        static constexpr double z_filter[lambda_arr_len] = 
+            {
+            0.0065, 0.0201, 0.0679, 0.2074, 0.6456,
+            1.3856, 1.7471, 1.7721, 1.6692, 1.2876,
+            0.8130, 0.4652, 0.2720, 0.1582, 0.0782,
+            0.0422, 0.0203, 0.0087, 0.0039, 0.0021,
+            0.0017, 0.0011, 0.0008, 0.0003, 0.0002,
+            0.0000, 0.0000, 0.0000, 0.0000, 0.0000,
+            0.0000, 0.0000, 0.0000, 0.0000, 0.0000,
+            0.0000, 0.0000, 0.0000, 0.0000, 0.0000,
+            0.
+            };
+
+        std::vector<std::vector<double>> xyz_to_rgb_matrix = {
+            std::vector<double>{3.2406, -1.5372, -0.4986},
+            std::vector<double>{-0.9689, 1.8758, 0.0415},
+            std::vector<double>{0.0557, -0.2040, 1.0570},
+        };
+
+        std::vector<double> light_intensity;
 
     private:
         World& world_;
--- src/LightSimulation.cpp
+++ src/LightSimulation.cpp
@@ -8,16 +8,14 @@ namespace Project {
     LightSource::LightSource(SourceType type, double intensity, double polarization_angle)
         : type_(type), intensity_(intensity), polarization_angle_(polarization_angle) {}
 
-    std::vector<std::array<LightBeam, 3>> LightSource::generate_beams() const {
-        std::vector<std::array<LightBeam, 3>> beams(GRID_SIZE * GRID_SIZE);
+    std::vector<std::array<LightBeam, LightSource::n_beams>> LightSource::generate_beams() const {
+        std::vector<std::array<LightBeam, LightSource::n_beams>> beams(GRID_SIZE * GRID_SIZE);
         JonesVector pol{std::cos(polarization_angle_ * M_PI / 180.0), 
                         std::sin(polarization_angle_ * M_PI / 180.0)};
         for (size_t i = 0; i < GRID_SIZE * GRID_SIZE; ++i) {
-            beams[i] = {
-                LightBeam(intensity_, 700.0, pol), // Red
-                LightBeam(intensity_, 545.0, pol), // Green
-                LightBeam(intensity_, 435.0, pol)  // Blue
-            };
+            for (size_t j = 0; j < LightSource::n_beams; ++j){
+                beams[i][j] = LightBeam(intensity_, PhysicalEngine::lambda_arr[j], pol);
+            }
         }
         return beams;
     }
@@ -53,7 +51,7 @@ namespace Project {
     void UniaxialCrystalVolume::apply_optical_effect(LightBeam& beam, double thickness) const {
         // Simplified birefringence: introduce phase difference
         double k = 2.0 * M_PI / beam.get_wavelength();
-        double n_e_theta = sqrt(
+        double n_e_theta = 1./sqrt(
             (cos(axis_theta_) * cos(axis_theta_)) / (n_o_ * n_o_) + 
             (sin(axis_theta_) * sin(axis_theta_)) / (n_e_ * n_e_) 
         );
@@ -61,7 +59,7 @@ namespace Project {
         double phase_diff = k * delta_n * thickness;
         
         JonesVector start_pol = beam.get_polarization();
-        JonesVector final_pol = beam.get_polarization();
+        JonesVector final_pol = beam.get_polarization();    
 
         // JonesMatrix = 
         // (A  B)
@@ -110,22 +108,52 @@ namespace Project {
     PhysicalEngine::PhysicalEngine(World& world, LightSource& light_source)
         : world_(world), light_source_(light_source) {}
 
+    std::vector<double> PhysicalEngine::apply_xyz_filter(std::vector<double> intensity){
+
+        std::vector<double> xyz_value(3, 0);
+        double delta_lambda = x_filter[1] - x_filter[0];
+        double normalization_sum_for_filters = 0;
+
+        for (size_t i = 0; i < LightSource::n_beams; ++i){
+            normalization_sum_for_filters += y_filter[i] * delta_lambda;
+            xyz_value[0]  +=  intensity[i] * x_filter[i] * delta_lambda;
+            xyz_value[1]  +=  intensity[i] * y_filter[i] * delta_lambda;
+            xyz_value[2]  +=  intensity[i] * z_filter[i] * delta_lambda;
+        }
+
+        xyz_value[0] *= 100/normalization_sum_for_filters;
+        xyz_value[1] *= 100/normalization_sum_for_filters;
+        xyz_value[2] *= 100/normalization_sum_for_filters;
+
+        return xyz_value;
+    }
+
     std::vector<std::vector<std::array<double, 3>>> PhysicalEngine::simulate() {
         auto beams = light_source_.generate_beams();
         std::vector<std::vector<std::array<double, 3>>> image(GRID_SIZE, 
             std::vector<std::array<double, 3>>(GRID_SIZE));
         
         size_t beam_idx = 0;
+        light_intensity.resize(LightSource::n_beams);
+
         for (int i = 0; i < GRID_SIZE; ++i) {
             for (int j = 0; j < GRID_SIZE; ++j) {
                 auto& volume = world_.get_volumes()[i][j];
                 if (!volume) continue;
 
-                for (int c = 0; c < 3; ++c) {
+                for (int c = 0; c < LightSource::n_beams; ++c) {
                     volume->apply_effect(beams[beam_idx][c]);
                     auto pol = apply_nicol_prism(beams[beam_idx][c].get_polarization());
-                    image[i][j][c] = beams[beam_idx][c].get_intensity() * 
-                                     (std::norm(pol[0]) + std::norm(pol[1]));
+                    light_intensity[c] = 
+                        beams[beam_idx][c].get_intensity() * (std::norm(pol[0]) + std::norm(pol[1]));
+                }
+                // after this cycle we will have vector of intense
+                std::vector<double> xyz_image = apply_xyz_filter(light_intensity);
+                for (size_t color = 0; color < 3; ++color){
+                    for (size_t k = 0; k < 3; ++k){
+                        image[i][j][color] += xyz_to_rgb_matrix[color][k] * xyz_image[k];
+                    }
+                    image[i][j][color] = std::round(image[i][j][color]);
                 }
                 ++beam_idx;
             }
@@ -134,10 +162,7 @@ namespace Project {
     }
 
     JonesVector PhysicalEngine::apply_nicol_prism(const JonesVector& input) const {
-        // Nicol prism at 90 degrees to initial polarization (assumed 0 degrees)
-        // JonesVector pol{input[0], input[1]}; // Passes all polarization
         JonesVector pol{0.0, input[1]}; // Passes only y-component
-        // JonesVector pol{input[0], 0.0}; // Passes only x-component
         return pol; 
     }
 
